0b4875920b4da153bd9f56954c291de2f68bcbe2
Sukhitha Saparamadu <saparamadusukhitha@gmail.com>
2025-05-05 16:05:53 +0530

    Refactor: Remove MovieApplication and update database access
    This commit removes the `MovieApplication` class and updates how the database is accessed.
    
    -   **Removed `MovieApplication`:** The `MovieApplication` class has been removed as it was no longer needed.
    -   **Database Access:** Changed the way the database is accessed since `MovieApplication` has been removed.
    -   **Gradle Settings:** Updated gradle settings.
    - **MovieDao:** Removed unused flow.

--- app/src/main/java/com/sukhitha/movieapp/MovieApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.sukhitha.movieapp
-
-import android.app.Application
-
-class MovieApplication : Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
-}
